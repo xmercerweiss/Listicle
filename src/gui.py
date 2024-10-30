1b81baa0b2c0d3ed39f2d1f0611d52d943d43f70
Xavier Mercerweiss <mercerweissx@gmail.com>
2024-10-30 08:18:33 -0600

    Project functional

--- src/gui.py
+++ src/gui.py
@@ -1,28 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
 
+import os
 
 class Listicle:
+
+    _TASKS_FILE = "./tasks"
 
     _ROOT_TITLE = "Listicle"
     _ROOT_GEO = "440x800"
     _ROOT_MIN_W = 440
     _ROOT_MIN_H = 50
 
-    _WIDGET_X_PAD = 2
+    _WIDGET_X_PAD = 3
+    _WIDGET_Y_PAD = 2
 
-    _BUTTON_TEXT = "+Add Task"
-    _BUTTON_FONT = ("Courier New", 12, "bold")
-    _BUTTON_FG = "#FFFFFF"
-    _BUTTON_BG = "#DA3A13"
+    _ENTRY_BUTTON_TEXT = "+Add Task"
+    _ENTRY_BUTTON_FONT = ("Courier New", 12, "bold")
+    _ENTRY_BUTTON_FG = "#FFFFFF"
+    _ENTRY_BUTTON_BG = "#DA3A13"
+    _ENTRY_BUTTON_BD = 5
     
+    _TASK_BUTTON_FONT = ("Courier New", 14)
+    _TASK_BUTTON_FG = "#FFFFFF"
+    _TASK_BUTTON_BG = "#DA3A13"
+
     _TEXTBOX_FONT = ("Courier New", 16, "bold")
 
     def __init__(self):
+        self._tasks = []
         self._root = self._init_root()
         self._entry_frame, self._tasks_frame = self._init_frames()
-        self._textbox = self._init_textbox()
-        self._populate_tasks_frame()
+        _, self._textbox = self._populate_entry_frame()
 
     def _init_root(self):
         root = tk.Tk()
@@ -64,11 +73,12 @@ class Listicle:
 
         return entry_frame, tasks_frame
 
-    def _init_textbox(self):
+    def _populate_entry_frame(self):
         textbox = tk.Entry(
             self._entry_frame,
             font=self._TEXTBOX_FONT
         )
+        textbox.bind("<Return>", self._add_task)
         textbox.grid(
             row=1,
             column=0,
@@ -76,25 +86,87 @@ class Listicle:
             padx=self._WIDGET_X_PAD
         )
 
-        button = tk.Button(
+        entry_button = tk.Button(
             self._entry_frame,
-            text=self._BUTTON_TEXT,
-            font=self._BUTTON_FONT,
-            fg=self._BUTTON_FG,
-            bg=self._BUTTON_BG
+            text=self._ENTRY_BUTTON_TEXT,
+            font=self._ENTRY_BUTTON_FONT,
+            fg=self._ENTRY_BUTTON_FG,
+            bg=self._ENTRY_BUTTON_BG,
+            bd=self._ENTRY_BUTTON_BD,
+            command=self._add_task
         )
-        button.grid(
+        entry_button.grid(
             row=1,
             column=1,
             sticky="nsew",
             padx=self._WIDGET_X_PAD
         )
 
-        return textbox
+        return entry_button, textbox
 
     def _populate_tasks_frame(self):
-        pass
+        for task in self._tasks:
+            self._build_task_button(task)
+
+    def _build_task_button(self, task):
+        button = tk.Button(
+            self._tasks_frame,
+            text=task,
+            font=self._TASK_BUTTON_FONT,
+            command=lambda: self._remove_task(task)
+        )
+
+        button.pack(
+            expand=False,
+            fill=tk.X,
+            padx=self._WIDGET_X_PAD,
+            pady=self._WIDGET_Y_PAD,
+            side=tk.TOP
+        )
+
+    def _add_task(self, *_):
+        # NOTE: Additional arguments are disregarded due to the extra
+        # argument passed by .bind() which is not passed elsewhere and
+        # is not used by this method.
+        task = self._textbox.get()
+        if task.strip() == "":
+            return
+        self._textbox.delete(0, tk.END)
+        self._tasks.append(task)
+        self._build_task_button(task)
+
+    def _remove_task(self, task):
+        # NOTE: In the event that mutiple tasks store exactly the same
+        # text, the first occurance will be deleted, not necessarily
+        # the one the user clicked on. This bug is so minor and unlikely
+        # to occur, and would require such significant restructuring,
+        # that it has not been patched.
+        if task not in self._tasks:
+            return
+        self._tasks.remove(task)
+        for child in self._tasks_frame.winfo_children():
+            if child.cget("text") == task:
+                child.destroy()
+                return
+
+    def _get_tasks(self):
+        if not os.path.isfile(self._TASKS_FILE):
+          open(self._TASKS_FILE, "a").close()
+          return
+        
+        self._tasks = []
+        with open(self._TASKS_FILE, "r") as file:
+            for line in file.readlines():
+                self._tasks.append(line.strip())
+
+    def _put_tasks(self):
+        with open(self._TASKS_FILE, "w") as file:
+            for task in self._tasks:
+                file.write(task + "\n")
 
     def mainloop(self):
+        self._get_tasks()
+        self._populate_tasks_frame()
         self._root.mainloop()
+        self._put_tasks()
 
